48edafea36cba628141e56d5d74fd57690b26524
Anshugarg007 <anshugargengg@gmail.com>
2022-06-20 01:41:17 +0200

    Initial commit Project set up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for creating a dynamic dashboard for analysing COVID-19 data such as infected and vaccinated cases over the time in different countries.',
+    author='Anshu_Garg',
+    license='MIT',
+)
